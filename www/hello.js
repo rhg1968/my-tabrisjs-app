96b90d9b5b67751c3bc468257fd2d8a2261cf755
Rob <robhg68@yahoo.com>
2015-04-04 14:34:01 -0400

    Tabris Test
    This is my first tabris app

---
+++ www/hello.js
@@ -0,0 +1,21 @@
+var page = tabris.create("Page", {
+  title: "Hello, World!",
+  topLevel: true
+});
+
+var button = tabris.create("Button", {
+  text: "Native Widgets",
+  layoutData: {centerX: 0, top: 100}
+}).appendTo(page);
+
+var label = tabris.create("TextView", {
+  font: "24px",
+  layoutData: {centerX: 0, top: [button, 50]}
+}).appendTo(page);
+
+button.on("selection", function() {
+  label.set("text", "Totally Rock!");
+  console.log('Hi There');
+});
+
+page.open();
